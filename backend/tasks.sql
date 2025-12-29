e64386dd2c2a9df688d35d04775714f1aed99f34
allen wale <jeffreyarevalo654@gmail.com>
2025-12-29 00:27:15 -0600

    first commit

---
+++ backend/tasks.sql
@@ -0,0 +1,7 @@
+CREATE TABLE task_tags (
+    task_id TEXT NOT NULL,
+    tag_id TEXT NOT NULL,
+    PRIMARY KEY (task_id, tag_id),
+    FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+);
